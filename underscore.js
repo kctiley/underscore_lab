90a3265ef571f2a445801329b2cc8e735eb99072
Klint Tiley <kctiley@gmail.com>
2015-08-04 23:20:57 -0600

    Initial commit and first solution

--- underscore.js
+++ underscore.js
@@ -1,1 +1,14 @@
 // PUT YOUR CODE HERE!!!
+//
+// average - returns the average value in a given array
+// _.average([4,6,1,1])
+// => 3
+var _ = {
+  average: function() {
+    var add = null;
+    for (i = 0; i < arguments.length; i++){
+      add = add + arguments[i];
+    }    
+    return add/arguments.length;
+  }
+};
