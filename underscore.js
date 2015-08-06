d64caa491bf964c8a312f60b2f254a10b5d5930c
Klint Tiley <kctiley@gmail.com>
2015-08-05 20:59:07 -0600

    Added fifth solution for "max" method

--- underscore.js
+++ underscore.js
@@ -29,7 +29,7 @@ var _ = {
 //first - returns the first element in an array
 //_.first([5, 4, 3, 2, 1]);
 //=> 5
- first: function(inputArray) {   
+  first: function(inputArray) {   
     return inputArray[0];
   },
 //
@@ -37,10 +37,26 @@ var _ = {
 // last - returns the last element in an array
 // _.last([5, 4, 3, 2, 1]);
 // => 1
- last: function(inputArray) {
+  last: function(inputArray) {
     return inputArray[inputArray.length - 1];    
   }
-
+//
+//
+//max - returns the maximum value in an array
+///var numbers = [10, 5, 100, 2, 1000];
+// _.max(numbers);
+// => 1000
+  max: function(inputArray) {
+    var result = [];
+    var highNumber = 0;
+    for (i = 0; i < inputArray.length; i++){
+      if (inputArray[i] > highNumber){
+        highNumber = inputArray[i];
+      };
+    };
+    result = highNumber;
+    return result;
+  },
 
 };
 
